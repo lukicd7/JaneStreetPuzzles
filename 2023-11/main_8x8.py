f75a67bf0b11622f0a007587c92aabece7582392
Dragan Lukic <lukicd7@gmail.com>
2023-11-24 16:25:16 -0500

    finalize 8x8 puzzle solution

--- 2023-11/main_8x8.py
+++ 2023-11/main_8x8.py
@@ -3,22 +3,16 @@
 # https://www.janestreet.com/puzzles/current-puzzle/
 #########################
 
-square = [[9,8,10,12,11,8,10,17],
-          [7,9,11,9,10,12,14,12],
-          [4,7,5,8,8,6,13,10],
-          [4,10,7,9,6,8,7,9],
-          [2,6,4,2,5,9,8,11],
-          [0,3,1,4,2,7,10,7],
-          [1,2,0,1,2,5,7,6],
-          [0,2,4,3,5,6,2,4]]
+square = [[9.,8.,10.,12.,11.,8.,10.,17.],
+          [7.,9.,11.,9.,10.,12.,14.,12.],
+          [4.,7.,5.,8.,8.,6.,13.,10.],
+          [4.,10.,7.,9.,6.,8.,7.,9.],
+          [2.,6.,4.,2.,5.,9.,8.,11.],
+          [0.,3.,1.,4.,2.,7.,10.,7.],
+          [1.,2.,0.,1.,2.,5.,7.,6.],
+          [0.,2.,4.,3.,5.,6.,2.,4.]]
 
-import numpy as np
-import sys
-
-# square = [[11,10,11,14],
-#           [8,6,9,9],
-#           [10,4,3,1],
-#           [7,6,5,0]]
+from copy import deepcopy
 
 num_visits = [ [ 0 for i in range(len(square)) ] for j in range(len(square)) ]
 num_visits[len(num_visits) - 1][0] = 1
@@ -35,12 +29,17 @@ def translate_diametric_pos(pos):
 
 
 def print_array(my_array):
-    # print("Total Duration: ", total_duration)
-    # print("Current State of Array:")
-    for row in my_array:
-        frmt = "{:>7}"*len(row) # trying to format the row data with 5 blank spaces
-        print(frmt.format(*(np.round(row,2)))) # makes it have nice layout as the numbers grow or shrink or have - sign
-    print("\n")
+    s = [[str(e) for e in row] for row in my_array]
+    lens = [max(map(len, col)) for col in zip(*s)]
+    fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+
+    x,y = translate_pos(cur_pos)
+    temp_array = deepcopy(my_array) # lists are mutable, need to deepcopy
+    temp_array[x][y] = '\033[91m' + str(temp_array[x][y]) + '\033[0m'
+
+    s = [[str(e) for e in row] for row in temp_array]
+    table = [fmt.format(*row) for row in s]
+    print ('\n'.join(table))
 
 def movement(duration, new_pos):
 
@@ -128,46 +127,70 @@ print_array(square)
 movement(0, "c2")
 movement(0, "a2")
 movement(9, "a3")
-movement(0, "a2")
-movement(0, "a1")
-movement(0, "b1")
-movement(0, "c1")
-movement(0, "e1")
-movement(4, "f1")
-movement(0, "f2")
-movement(0, "e2")
-movement(0, "d4")
+movement(0, "a4")
+movement(0, "a5")
+movement(0, "c4")
 movement(0, "e4")
-movement(0, "e5")
-movement(0, "f5")
-movement(18, "g5")
+movement(0, "f2")
+movement(0, "f3")
+movement(0, "g5")
 movement(0, "h5")
 movement(6, "h6")
 movement(0, "g8")
 movement(0, "e8")
-movement(9, "c7")
-
-# start a loop to gain duration
-
-movement(0, "d5")
-movement(0, "e5")
-movement(0, "g5")
-movement(56, "f3")
-movement(0, "e3")
-movement(0, "e4")
-movement(0, "e5")
-movement(0, "d5")
 movement(0, "c7")
-movement(0, "e8")
+
+movement(15, "a7")
+movement(0, "c7")
+movement(0, "d7")
+movement(12, "b6")
+movement(0, "d7")
+movement(76, "c5") # dragan thinks the trick might be dropping the 8 & 7s to a 1 instead of a 3
+movement(0, "b3")
+movement(0, "b1")
+movement(0, "c1")
+movement(0, "e1")
+movement(0, "e2")
+movement(5, "d4")
+movement(0, "c4")
+movement(0, "a5")
+movement(0, "c5")
+movement(0, "c6")
+
+# issue on a5, c5, c4
+# trying to insert a new move set to wait on 3s in cell d6
+# movement(3, "a5")
+# movement(0, "c4")
+# movement(0, "d4")
+
+# have to take this path to reduce steps from 4x to 3x
+movement(3, "e6")
+movement(0, "e4")
+movement(0, "d4")
+
+
+movement(0, "d6")
+movement(60, "b7")
+movement(0, "c5")
+movement(0, "e6")
+movement(0, "f4")
+movement(0, "d5")
+movement(0, "d4")
+movement(0, "c4")
+movement(0, "a5")
+movement(0, "c6")
+
+# resume the rest of the run
+movement(0, "c7")
+movement(0, "d7")
 movement(0, "e7")
-
-# finish loop to gain duration
-# may want to enter via d7 instead of e8 though, so the first line below may need to be removed
-
 movement(0, "f7")
 movement(0, "g7")
 movement(1, "h7")
 movement(0, "h8")
 
-
 print_array(num_visits)
+
+# other ideas
+# 1: get a different number to 17, then all the 17's to ~15's, then all the ~15's to ~13's, etc.
+# until we have almost ALL ~5's and then we just hop kiddie corners around to get to H8 which is ~5
